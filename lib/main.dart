28e251ddec2f88749a2c7ec723e79bbebc40abed
PC LAB <pclab@istinye.edu.tr>
2022-12-20 17:52:49 +0300

    bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,29 @@
-
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:dororo/UrunDetay.dart';
 import 'package:dororo/anasayfa/Anasayfa.dart';
 import 'package:dororo/giris.dart';
 import 'package:flutter/material.dart';
+
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Detay()
-    );
+    return MaterialApp(home: giris());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-    
-    );
+    return Scaffold();
   }
 }
